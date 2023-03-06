74155d1a2e851e5482576b6aaed4037fce98b7a7
Yash Kanekar <kanekaryash.11@gmail.com>
2023-03-06 21:55:17 +0530

    initialized routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import CategoryItem from "./components/category-item/CategoryItem.component";
-import Directory from "./components/directory/Directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/routes/Home.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
