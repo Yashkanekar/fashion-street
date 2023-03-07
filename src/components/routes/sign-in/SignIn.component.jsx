bf434201f4513b2729dae55828241d041c24ec32
Yash Kanekar <kanekaryash.11@gmail.com>
2023-03-07 19:17:41 +0530

    completed signin component, added firebase libraries and firebase utils

--- src/components/routes/sign-in/SignIn.component.jsx
+++ src/components/routes/sign-in/SignIn.component.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  return <h2>This is the signin page</h2>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(response.user);
+  };
+
+  return (
+    <div>
+      <h2>This is the signin page</h2>
+      <button onClick={logGoogleUser}>Sign in with google popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getDoc, setDoc, doc, getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIFoU4DjQYNseOTYtov-FzsDvZbEoPKfU",
+  authDomain: "fashion-street-db.firebaseapp.com",
+  projectId: "fashion-street-db",
+  storageBucket: "fashion-street-db.appspot.com",
+  messagingSenderId: "722674903650",
+  appId: "1:722674903650:web:2b90343e434d151356ff3b",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+//create a db instance of firestore db
+const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid); //takes in the database, the collection and the unique document identifier
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("An error occured while creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
