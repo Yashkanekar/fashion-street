6862840faa851a15ffd5e1fe90f51297c15bc1ba
Yash Kanekar <kanekaryash.11@gmail.com>
2023-03-14 02:46:38 +0530

    added cartcount functionality

--- src/components/cart-icon/CartIcon.component.jsx
+++ src/components/cart-icon/CartIcon.component.jsx
@@ -4,14 +4,20 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  const cartItemsQuantity = (cartItems) => {
+    cartItems.reduce((accumulator, cartItem) => {
+      return accumulator + cartItem.quantity;
+    }, 0);
+  };
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
