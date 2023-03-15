64af2de978a5038ce98b68d30c824b13662d6c63
Yash Kanekar <kanekaryash.11@gmail.com>
2023-03-15 20:31:41 +0530

    component: categories preview

--- src/components/routes/shop/Shop.component.jsx
+++ src/components/routes/shop/Shop.component.jsx
@@ -1,15 +1,22 @@
-import React, { useContext } from "react";
-import { CategoriesContext } from "../../../contexts/CategoriesMapContext";
-import ProductCard from "../../product-card/ProductCard.component";
+import React, { Fragment, useContext } from "react";
+import { CategoriesContext } from "../../../contexts/CategoriesContext";
+import CategoryPreview from "../../category-preview/CategoryPreview.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
+
   return (
-    <div className="products-container">
-      {/* {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))} */}
+    <div className="shop-container">
+      {
+        // "Object.keys(categoriesMap)"  gives back an array of all the key values from 'categoriesMap' object.
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      }
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/UserContext";
-import { CategoriesProvider } from "./contexts/CategoriesMapContext";
+import { CategoriesProvider } from "./contexts/CategoriesContext";
 import { CartProvider } from "./contexts/CartContext";
 import reportWebVitals from "./reportWebVitals";
 
